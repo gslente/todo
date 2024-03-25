f121d958dff554105927d9187dbe0fd41940927f
gslente <151698739+gslente@users.noreply.github.com>
2024-03-25 12:58:29 +0800

    Update frontend

--- frontend/src/components/TodoRow.jsx
+++ frontend/src/components/TodoRow.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
-import TodoCheckbox from './TodoCheckbox'
+import { React, useEffect } from 'react'
+import useState from 'react-usestateref'
+import axios from 'axios'
 
-const TodoRow = ({ body }) => {
+var URL = 'http://127.0.0.1:8000/api/todo/'
+
+const TodoRow = ({ item, get_todo_list }) => {
+  const [todo, set_todo, todo_ref] = useState({
+    id: item.id,
+    title: item.title,
+    body: item.body,
+    is_complete: item.is_complete,
+  })
+
+  const a_onClick = (event) => {
+    event.preventDefault()
+    delete_todo(item.id)
+  }
+
+  const input_checkbox_onClick = (event) => {
+    set_todo({
+      ...todo,
+      is_complete: event.target.checked
+    })
+
+    update_todo(item.id)
+  }
+
+  const input_text_onChange = (event) => {
+    set_todo({
+      ...todo,
+      body: event.target.value
+    })
+
+    update_todo(item.id)
+  }
+
+  const input_text_onBlur = () => {
+    update_todo(item.id)
+  }
+
+  const update_todo = async (id) => {
+    let update_url = URL.concat(id, '/')
+    await axios.put(update_url, todo_ref.current)
+      .then(response => {
+        const data = response.data
+      })
+      .catch(error => console.log(error))
+
+    get_todo_list()
+  }
+
+  const delete_todo = async (id) => {
+    let delete_url = URL.concat(id, '/')
+
+    await axios.delete(delete_url)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(event => {
+        console.log(event)
+      })
+
+    get_todo_list()
+  }
+
+  /*useEffect(() => {
+    console.log(todo_ref.current)
+
+    return () => {
+      console.log(todo_ref.current)
+    }
+  }, [])*/
+
   return (
     <td>
-      <TodoCheckbox /> <input type='text' defaultValue={ body } />
+      <input type='checkbox' defaultChecked={item.is_complete} onChange={input_checkbox_onClick} />
+      <input type='text' defaultValue={item.body} onChange={input_text_onChange} onBlur={input_text_onBlur} />
+      <a href="#" onClick={a_onClick}>delete</a>
     </td>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,36 @@
-import { React } from 'react'
+// CONTRIB
+import { React, useEffect, useState } from 'react'
+import axios from 'axios'
+
+// CUSTOM
 import Header from '../components/Header'
 import Table from '../components/Table'
+import TodoForm from '../components/TodoForm'
+
+const URL = 'http://127.0.0.1:8000/api/todo/'
 
 const Home = () => {
+  const [todo_list, set_todo_list] = useState([])
+
+  const get_todo_list = async () => {
+    await axios.get(URL)
+      .then(response => {
+        const data = response.data
+        set_todo_list(data)
+      })
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    get_todo_list()
+  }, [])
+
   return (
     <>
       <Header />
       <main>
-        <Table/>
+        <TodoForm get_todo_list={get_todo_list} />
+        <Table todo_list={todo_list} get_todo_list={get_todo_list} />
       </main>
     </>
   )
